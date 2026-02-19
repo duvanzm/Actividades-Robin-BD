fb2d48ae67415315eced38e8b094983f2eb83702
duvanzm <dazm0998@gmail.com>
2026-02-19 17:24:00 -0500

    ad

--- actividad-3/express/server.js
+++ actividad-3/express/server.js
@@ -56,7 +56,7 @@ app.get(`/${urllev}1/${urlEjer}1`, (req, res) => {
   });
 });   
 
-//2. Obtener todos los pedidos (código y fecha) realizados por un usuario con un correo electrónico específico (ej: isamel@pedrito.es).
+// 2. Obtener todos los pedidos (código y fecha) realizados por un usuario con un correo electrónico específico (ej: isamel@pedrito.es).
 
 app.get(`/${urllev}1/${urlEjer}2`, (req, res) => {
   const query = "SELECT o.order_number,o.created_at FROM orders o INNER JOIN users u ON o.user_id = u.id WHERE u.email = 'isamel@pedrito.es'";
@@ -70,6 +70,45 @@ app.get(`/${urllev}1/${urlEjer}2`, (req, res) => {
   });
 });  
 
+// 3. Mostrar el nombre de cada producto junto con el nombre de la categoría a la que pertenece.
+
+app.get(`/${urllev}1/${urlEjer}3`, (req, res) => {
+  const query = "SELECT p.name AS product_name, c.name AS category_name FROM products p INNER JOIN categories c ON p.category_id = c.id";
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error al ejecutar la consulta:', err);
+      return res.status(500).send('Error en el servidor');
+    }
+    console.log(res.json(results)); 
+  });
+}); 
+
+// 4.Obtener una lista de los usuarios que se han registrado en el sistema pero que nunca han realizado una compra.
+app.get(`/${urllev}1/${urlEjer}4`, (req, res) => {
+  const query = "SELECT u.name,u.email FROM users u LEFT JOIN orders o ON u.id = o.user_id WHERE o.user_id IS NULL  ";
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error al ejecutar la consulta:', err);
+      return res.status(500).send('Error en el servidor');
+    }
+    console.log(res.json(results)); 
+  });
+}); 
+
+// 5.Calcular el monto total gastado de un usuario (el que ustedes elijan) en toda su historia, mostrando el nombre del usuario y el total.
+app.get(`/${urllev}1/${urlEjer}5`, (req, res) => {
+  const query = "SELECT u.name, SUM(o.total_amount) AS total_gastado FROM users u LEFT JOIN orders o ON u.id = o.user_id GROUP BY u.id HAVING u.id = '1' ";
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error('Error al ejecutar la consulta:', err);
+      return res.status(500).send('Error en el servidor');
+    }
+    console.log(res.json(results)); 
+  });
+}); 
 
 const PORT = process.env.PORT || 3000;
 
